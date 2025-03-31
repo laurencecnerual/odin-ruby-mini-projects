9044ce457cf3329ad747f03db13277d53df8584f
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-03-31 18:15:02 +0900

    Feat: Implements Caesar Cipher

---
+++ caesar-cipher/caesar-cipher.rb
@@ -0,0 +1,33 @@
+def caesar_cipher(plainText, shiftCount) 
+  lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
+  uppercaseAlphabet = lowercaseAlphabet.upcase
+  alphabetCount = uppercaseAlphabet.length
+
+  adjustedShiftCount = shiftCount % alphabetCount
+  cipherText = ""
+  plainCharacters = plainText.split("")
+
+  for character in plainCharacters do
+    if lowercaseAlphabet.include?(character)
+      newCharacterIndex = (lowercaseAlphabet.index(character) + adjustedShiftCount) % alphabetCount
+      cipherText += lowercaseAlphabet[newCharacterIndex]
+    elsif uppercaseAlphabet.include?(character)
+      newCharacterIndex = (uppercaseAlphabet.index(character) + adjustedShiftCount) % alphabetCount
+      cipherText += uppercaseAlphabet[newCharacterIndex]
+    else
+      cipherText += character
+    end
+  end
+
+  cipherText
+end
+
+puts caesar_cipher("What a string!", 0)
+puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 31)
+puts caesar_cipher("What a string!", -21)
+puts caesar_cipher("What a string!", -47)
+puts caesar_cipher("What a string!", 1)
+puts caesar_cipher("What a string!", -25)
+puts caesar_cipher("What a string!", -1)
+puts caesar_cipher("What a string!", 25)
