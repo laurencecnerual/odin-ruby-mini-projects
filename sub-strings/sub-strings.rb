2720387f7348ac17c7b3922f881a4539fecc59cd
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-03-31 19:41:59 +0900

    Feat: Implements Sub Strings

---
+++ sub-strings/sub-strings.rb
@@ -0,0 +1,34 @@
+def substrings(sentence, wList)
+  occurrencesPerWord = Hash.new(0)
+  words = sentence.split(" ")
+
+  for word in words
+    word = word.gsub(/[!?.,;():]/, "").gsub("'s", "").downcase
+
+    if wList.include?(word)
+      occurrencesPerWord[word] += 1
+    end
+  end
+
+  # to find and add wList words that are substrings of other words
+  foundWords = occurrencesPerWord.keys
+
+    for element in wList
+      for word in foundWords
+        if word.include?(element) && word != element
+          occurrencesPerWord[element] += 1
+        end
+      end
+    end
+
+   occurrencesPerWord
+end
+
+# example usage
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# puts substrings("below", dictionary)
+# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+# example output
+# {"below"=>1, "low"=>1}
+# {"howdy"=>1, "partner"=>1, "sit"=>1, "down"=>1, "how"=>2, "it"=>2, "going"=>1, "go"=>1, "i"=>3, "own"=>1, "part"=>1}
